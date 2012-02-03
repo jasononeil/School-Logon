d3a2187076d80aa7b0206e27c77d0f0d103aae40
Jason O'Neil <jason.oneil@gmail.com>
2012-02-03 15:08:06 +0800

    Added actual command that should mount for OSX

--- Logon.py
+++ Logon.py
@@ -1,51 +1,64 @@
 import subprocess
 import sys
 import re
+import os
 from datetime import date
 
 def runCommand(cmd):
-	#sys.stderr = sys.stdout
-	#proc = error=subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-	#.communicate()[0].decode("windows-1252")
-	#return_code = proc.wait()
-	#output = ""
-	#for line in proc.stdout:
-		#output = output + "stdout: " + line.decode("windows-1252").rstrip() + "\n"
-	#for line in proc.stderr:
-		#output = output + "stderr: " + line.decode("windows-1252").rstrip() + "\n"
-	#return output
+	sys.stderr = sys.stdout
+	print cmd
+	proc = error=subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0].decode("windows-1252")
+	return_code = proc.wait()
+	output = ""
+	for line in proc.stdout:
+		output = output + "stdout: " + line.decode("windows-1252").rstrip() + "\n"
+	for line in proc.stderr:
+		output = output + "stderr: " + line.decode("windows-1252").rstrip() + "\n"
+	return output
 	pass
 	
 
 def disconnectDrives():
 	print ("Disconnecting existing drives...");
-	cmd = "net use * /delete /yes"
-	runCommand(cmd)
+	#cmd = "net use * /delete /yes"
+	#runCommand(cmd)
 	
 def mapDrive(driveLetter, server, share, username, password):
 	# map drive
 	note = ("Loading " + driveLetter + " drive:  ")
+	home = os.getenv('USERPROFILE') or os.getenv('HOME')
+	networkDir = home + "NetworkDrives/"
+	
+	# If we're on Linux
 	if sys.platform == "linux2":
-		cmd = "mount.cifs //" + server + "/" + share + " /home/jason/NetworkDrives/" + driveLetter + " -o user=" + username + ",pass=" + password
+		directory = networkDir + driveLetter
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		cmd = "mount.cifs //" + server + "/" + share + " " + directory + " -o user=" + username + ",pass=" + password
 		output = runCommand(cmd)
 		print output
+
+	# If we're on Windows
 	elif sys.platform == "win32": 
 		cmd = "net use " + driveLetter + ": \\\\" + server + "\\" + share + " /user:" + username + " " + password + " /PERSISTENT:No"
 		output = runCommand(cmd)
 		if 'success' in output:
 	    		result = "SUCCESS"
-
 		elif 'denied' in output:
 				result = "DENIED"
-
 		elif "not found" in output:
 				result = "NOTFOUND"
-
 		else:
 				result = "FAILEDUNKNOWN"
-
+	
+	# If we're on OSX
 	elif sys.platform == "darwin":
-		pass
+		directory = networkDir + driveLetter
+		if not os.path.exists(directory):
+			os.makedirs(directory)
+		cmd = "mount_smbfs //" + username + ":" + password + "@" + server + "/" + share + " " + directory
+		output = runCommand(cmd)
+		print output
 	#
 	
 
@@ -79,7 +92,6 @@ def connectToStaffPWSDrives(username, password):
 	mapDrive("R", "192.168.0.5", "accounts", username, password);
 	mapDrive("W", "192.168.0.5", "seniorstaff", username, password);
 	# Maze drive?  How can we map, only if one of the preceeding worked?  Say, office
-	#mapDrive("", "192.168.155.172", "maze", "Administrator", "maiLBill13");
 
 	print ("Done");
 
